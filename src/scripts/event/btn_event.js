a3a382f1d933de64eac4e1aa4d25f0dedc597361
Leorna <gabrielcapuerj@gmail.com>
2019-09-14 16:33:24 -0300

    removendo zoom

--- src/scripts/event/btn_event.js
+++ src/scripts/event/btn_event.js
@@ -1,11 +1,10 @@
 'use strict';
 import MouseAction from "./scripts/components/MouseAction";
 import { drawSimple, drawDuo, drawTriple } from './scripts/functions/ligacoes';
-import {SimpleConnection, DuoConnection, TripleConnection} from "./scripts/components/Buttons";
 
 let mouse = new MouseAction();
 let ctx = $("#canvas")[0].getContext('2d');
-let buttonsObjects = new Array;
+
 
 
 $(".btn-toolbar").click(function(){
@@ -15,47 +14,13 @@ $(".btn-toolbar").click(function(){
 
 $("#canvas").click(function(event){
     console.log(mouse.currentClick);
-    if (mouse.currentClick == "btnSimple"){       
-        let simple = new SimpleConnection(event.offsetX, event.offsetY, ctx);
-        simple.draw();
-        buttonsObjects.push(simple);
+    if (mouse.currentClick == "btnSimple"){  
+        drawSimple(ctx, event.offsetX, event.offsetY);     
     }
     else if (mouse.currentClick == "btnDuo"){
-        let duo = new DuoConnection(event.offsetX, event.offsetY, ctx);
-        duo.draw();
-        buttonsObjects.push(duo);
+        drawDuo(ctx, event.offsetX, event.offsetY);
     }
     else if (mouse.currentClick == "btnTriple"){
-        let triple = new TripleConnection(event.offsetX, event.offsetY, ctx);
-        triple.draw();
-        buttonsObjects.push(triple);
+        drawTriple(ctx, event.offsetX, event.offsetY);
     } 
-
-    console.log(buttonsObjects);
-});
-
-function redraw(zoom) {
-    for (let button of buttonsObjects)
-        button.redraw(zoom);
-}
-
-let scaleNumber;
-
-$(function() {
-    $("#1x").click(() => {
-        if (typeof scaleNumber === 'number')
-            redraw(1 / scaleNumber);
-    });
-    $("#2x").click(() => { 
-        scaleNumber = 2;
-        redraw(scaleNumber);
-    });
-    $("#3x").click(() => { 
-        scaleNumber = 3;
-        redraw(scaleNumber); 
-    });
-    $("#4x").click(() => { 
-        scaleNumber = 4;
-        redraw(scaleNumber); 
-    });
 });
